8d0cc2121f33871bfadea5561864d8f6c4d6f75e
Adrian <NAdrian@cvent.com>
2019-04-15 21:47:58 -0400

    actually commit the working changes

---
+++ squarewrapperapp/app/src/main/java/gmu/rqr/square_wrapper_app/util/AnimationListenerAdapter.java
@@ -0,0 +1,21 @@
+package gmu.rqr.square_wrapper_app.util;
+
+import android.animation.Animator;
+
+public abstract class AnimationListenerAdapter implements Animator.AnimatorListener {
+    @Override public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override public void onAnimationRepeat(Animator animation) {
+
+    }
+}
